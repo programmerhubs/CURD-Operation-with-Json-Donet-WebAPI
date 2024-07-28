7ad4190195bf2ad9a7df444bee60c085a7367388
rohit sharma <rohitk260123@outlook.com>
2024-07-28 07:20:43 +0530

    Project Setup

---
+++ CURDWithJson/DomainLayer/Entities/Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entities
+{
+    public  class Image
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Enter Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Select Valid  Date")]
+        
+        public DateTime CreatedOn { get; set; }
+        [Required(ErrorMessage = "Mandatory*")]
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Mandatory*")]
+        [Url(ErrorMessage ="Enter vaild URL")]
+        public string URL { get; set; }
+    }
+}
