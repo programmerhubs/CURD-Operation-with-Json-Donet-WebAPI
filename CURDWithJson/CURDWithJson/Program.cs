3f095e10ef93052a15523479d1b3a4fde4f150ce
rohit sharma <rohitk260123@outlook.com>
2024-07-28 08:55:33 +0530

    Exception Handling

--- CURDWithJson/CURDWithJson/Program.cs
+++ CURDWithJson/CURDWithJson/Program.cs
@@ -1,3 +1,4 @@
+using CURDWithJson.Middleware;
 using ServiceLayer;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<JsonFileService>(_=>new JsonFileService("ImageDetails.json"));
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
